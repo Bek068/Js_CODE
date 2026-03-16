9ea338827722f33a7a2e60cd20a972977ff3f88a
Bek068 <rijal.bebek68@gmail.com>
2026-03-16 11:18:07 +0545

    tatype

---
+++ comparison.js
@@ -0,0 +1,6 @@
+// console.log(2>1) //true
+
+console.log("2">1)  //It returns true because when we compare a string with a number, JavaScript tries to convert the string to a number. In this case, "2" is converted to the number 2, and since 2 is greater than 1, the expression evaluates to true.
+
+console.log("2"===2) //It returns false because the strict equality operator (===) checks for both value and type. In this case, "2" is a string and 2 is a number, so they are not strictly equal, resulting in false.
+
