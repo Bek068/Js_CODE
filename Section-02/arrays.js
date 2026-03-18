e062d3394a7e466a3e90f4a7feb2261dab634faa
Bek068 <rijal.bebek68@gmail.com>
2026-03-18 08:50:57 +0545

    JavaScript Day-14 commit

---
+++ Section-02/arrays.js
@@ -0,0 +1,37 @@
+//Arrays...
+//Arrays are a data structure that can hold multiple values. They are ordered and indexed, meaning that each value has a specific position in the array.
+
+//Creating an Array..
+
+const myArr = [1, 2, 3, "Apple", "Mango", "Document", "Intesteller"]
+
+//Accessing Array Elements..
+
+console.log(myArr[0]) // 1;
+console.log(myArr[2]) // 3;
+myArr.push("Banana");// Adding an element to the end of the array
+console.log(myArr);
+console.log(myArr.pop()) // Removing the last element from the array
+console.log(myArr);
+myArr.unshift("Strawberry") // Adding an element to the beginning of the array
+console.log(myArr);
+myArr.shift() // Removing the first element from the array
+console.log(myArr);
+
+console.log(myArr.includes("Mango")) //Returns true if the array contains the specified element, otherwise returns false
+
+console.log(myArr.indexOf("Document")) //Returns the first index at which a given element can be found in the array, or -1 if it is not present.
+
+console.log(myArr.length) //Returns the number of elements in the array.
+
+console.log(myArr)
+
+const newArr =myArr.join() //Joins all elements of an array into a string and returns this string. The default separator is a comma (,), but you can specify a different separator as an argument.
+
+console.log(myArr);
+
+console.log(newArr); //Returns a string that is the result of concatenating all the elements in the array, separated by commas (or the specified separator).
+
+console.log(typeof newArr) //Returns the type of the variable, which is a string in this case.
+
+console.log(myArr.slice(2, 4)) //Returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included). The original array will not be modified.
